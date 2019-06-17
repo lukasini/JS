966d43f64384fa210e7d7c62cca86c6abb524c87
lukasini <lukasvarhol@yahoo.com>
2019-06-17 20:14:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,7 @@ function calculateNetto()
         num1 = document.getElementById("sallBrutto").value;
         num2 = document.getElementById("calculateChildren").value;
         num3 = 100;
-        document.getElementById("result").innerHTML = (num1 / num3) * num2;
+        num4 = 22;
+        num5 = 67.6;
+        document.getElementById("result").innerHTML = num5 * (num1 / num3) - (num4 * num2);
 }
